5c19f622cc5a280565dc075204639e6236bc2fbb
Shah Fazle Elahi <shahfazleelahi@Shah-Fazle-Elahi.local>
2024-09-23 03:18:32 +0600

    first commit

---
+++ while/3 sum of odd numbers.js
@@ -0,0 +1,18 @@
+
+
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 81 to 131.
+
+ */
+/***
+
+Subtask-2:
+
+Display sum of all the even numbers from 206 to 311.
+
+ */
+
+/*programming hero*/
